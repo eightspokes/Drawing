29076918b52e94cc00d5e034c4c024f3e0523f1c
romanmuni8 <romanmuni8@gmail.com>
2022-12-01 10:46:56 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Roman on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
